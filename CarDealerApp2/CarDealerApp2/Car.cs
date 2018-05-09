21fe57e7c58d7924a9cd9ed7ab16703e1e6b4f58
Dariusz Bożek <dariusz.bozek@mbdvc.pl>
2018-05-09 11:42:33 +0200

    cd1

---
+++ CarDealerApp2/CarDealerApp2/Car.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealerApp2
+{
+    public class Car
+    {
+        public string Marka { get; set; } //tworzymy własciwości
+        public int Rocznik { get; set; }
+        public string TypSilnika { get; set; }
+        public int Cena { get; set; }
+
+
+        public void Wypisz()
+        {
+            Console.WriteLine("Marka: " + this.Marka, "Rocznik: " + this.Rocznik, "Typ Silnika: " + this.TypSilnika, "Cena: " + this.Cena);
+        }
+
+        public Car(string Car, int Year, string E, int Price) //tworzymy konstruktor w z 4 parametrami które muszą być przekazane
+        {
+            Marka = Car;
+            Rocznik = Year;
+            TypSilnika = E;
+            Cena = Price;
+        }
+
+        public Car()
+        {
+            Marka = "Fiat Punto";
+            Rocznik = 2005;
+            TypSilnika = "Benzyna";
+            Cena = 4000;
+        }
+
+        public Car(string Car)
+        {
+            Marka = Car;
+            Rocznik = 2005;
+            TypSilnika = "Benzyna";
+            Cena = 4000;
+
+        }
+    }
+}
--- CarDealerApp2/CarDealerApp2/Program.cs
+++ CarDealerApp2/CarDealerApp2/Program.cs
@@ -10,6 +10,31 @@ namespace CarDealerApp2
     {
         static void Main(string[] args)
         {
+            List<Car> ListaSamochodow = new List<Car>();
+            ListaSamochodow.Add(new Car("Ford ", 2005, "Benzyna ", 4000));
+            ListaSamochodow.Add(new Car("BMW ", 2008, "Dizel ", 5000));
+
+            foreach (Car iCar in ListaSamochodow)
+            {
+                Console.WriteLine("LISTA SAMOCHODOW:");
+                Console.WriteLine("Marka: " + iCar.Marka +
+                    "\nRocznik: " + iCar.Rocznik +
+                    "\nTyp Silnika: " + iCar.TypSilnika +
+                    "\nCena: " + iCar.Cena + "PLN");
+            }
+
+            
+
+ 
+
+            Car Car1 = new Car("Audi");
+            Car Car2 = new Car();
+            Car Car3 = new Car("Kia", 2008, "Diezel", 4000);
+            Car1.Wypisz();
+            Car2.Wypisz();
+            Car3.Wypisz();
+
+            Console.ReadKey();
         }
     }
 }
